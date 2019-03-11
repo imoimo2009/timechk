19a3720c0f1c0c47f9ffce883d7c148d226008c4
Kouji Imanishi <imoimo@outlook.com>
2019-03-11 17:01:23 +0900

    とりあえず完成

--- timechk/timechk.cpp
+++ timechk/timechk.cpp
@@ -4,58 +4,53 @@
 #include <time.h>
 
 #define STR_LENGTH 256
-#define DATA_LENGTH 100
+#define DAT_LENGTH 100
 
 #define ERR_STRING "エラー：%s がありません。"
+#define USG_STRING "USAGE:timechk.exe <IP Address>"
+
+// グローバル変数定義
+char Data[DAT_LENGTH][STR_LENGTH];
 
 // iniファイルを読み込む
-void load_data(char data[DATA_LENGTH][STR_LENGTH],char *path)
+int load_data(char *path)
 {
 	FILE *fp;
 	errno_t err;
+	char tmp[STR_LENGTH];
 	int i = 0;
 
 	err = fopen_s(&fp, path, "r");
-	if (err != 0) return;
-	while (fgets(data[i], STR_LENGTH, fp) != NULL) {
+	if (err != 0) return err;
+	while (fgets(tmp, STR_LENGTH, fp) != NULL) {
 		int j = 0;
-		while (data[i][j] != '\0' && j < STR_LENGTH) {
-			if (data[i][j] == '\n') data[i][j] = '\0';
-			j++;
+		if (tmp[0] != '#' && strlen(tmp) > 1) {
+			while (tmp[j] != '\0' && j < STR_LENGTH) {
+				if (tmp[j] == '\n') tmp[j] = '\0';
+				j++;
+			}
+			strcpy_s(Data[i], STR_LENGTH, tmp);
+			if (i++ >= DAT_LENGTH) break;
 		}
-		if (data[i][0] != '#' && strlen(data[i]) > 0) i++;
-		if (i >= DATA_LENGTH) break;
 	}
 	fclose(fp);
-	while (i < DATA_LENGTH) data[i++][0] = '\0';
+	return 0;
 }
 
-// 時刻文字列を作成
-void time_str(char *str,tm lt)
-{
-	char num[STR_LENGTH];
-
-	sprintf_s(num, STR_LENGTH, "%02d", lt.tm_hour);
-	strcpy_s(str, STR_LENGTH, num);
-	strcat_s(str, STR_LENGTH, ":");
-	sprintf_s(num, STR_LENGTH, "%02d", lt.tm_min);
-	strcat_s(str, STR_LENGTH, num);
-}
-
-// 現在時刻文字列を取得
-void now_str(char *str)
+// 現在時刻を分に変換して返す
+int now(void)
 {
 	time_t t;
-	tm *lt = new tm;
-	
+	tm lt;
+
 	t = time(NULL);
-	localtime_s(lt, &t);
-	time_str(str, *lt);
-	delete(lt);
+	localtime_s(&lt, &t);
+	return lt.tm_hour * 60 + lt.tm_min;
 }
 
 // ファイル名（拡張子無し）を作成する
-void get_basename(char *path,const char *src) {
+void get_basename(char *path,const char *src)
+{
 	char *p, *s, *r = NULL;
 
 	strcpy_s(path, STR_LENGTH, src);
@@ -77,75 +72,116 @@ void get_ip_address(char *str, char *data)
 	strcpy_s(str, STR_LENGTH, strtok_s(str, ",", &p));
 }
 
-// データ文字列から開始時刻を取得
-void get_start_time(char *str, char *data)
+// 時刻文字列から分に変換
+int str2min(char *str)
+{
+	char tmp[STR_LENGTH], *tok, *p = NULL;
+	int min;
+
+	strcpy_s(tmp, STR_LENGTH, str);
+	tok = strtok_s(tmp, ":", &p);
+	min = atoi(tok) * 60;
+	tok = strtok_s(NULL, ":", &p);
+	min += atoi(tok);
+	return min;
+}
+
+// 分から時刻文字列に変換
+void min2str(char *str,int min)
+{
+	char tmp[STR_LENGTH];
+
+	sprintf_s(str, STR_LENGTH, "%02d", min / 60);
+	sprintf_s(tmp, STR_LENGTH, "%02d", min % 60);
+	strcat_s(str, STR_LENGTH, ":");
+	strcat_s(str, STR_LENGTH, tmp);
+}
+
+// データ文字列から開始時刻(分)を取得
+int get_start_time(char *data)
 {
 	char *p = NULL;
+	char str[STR_LENGTH];
 
 	strcpy_s(str, STR_LENGTH, data);
 	strtok_s(str, ",", &p);
 	strcpy_s(str, STR_LENGTH, strtok_s(NULL, ",", &p));
+	return str2min(str);
 }
 
-// データ文字列から終了時刻を取得
-void get_end_time(char *str, char *data)
+// データ文字列から終了時刻(分)を取得
+int get_end_time(char *data)
 {
 	char *p = NULL;
+	char str[STR_LENGTH];
 
 	strcpy_s(str, STR_LENGTH, data);
 	strtok_s(str, ",", &p);
 	strtok_s(NULL, ",", &p);
 	strcpy_s(str, STR_LENGTH, strtok_s(NULL, ",", &p));
+	return str2min(str);
 }
 
 // IPをもとに時間のチェックを行う
-int chktime(char data[DATA_LENGTH][STR_LENGTH],char *ip)
+int chktime(char *ip)
 {
-	char now[STR_LENGTH],tgt[STR_LENGTH];
-	int i;
+	char tgt_ip[STR_LENGTH];
+	int i = 0,now_min;
 
-	
+	now_min = now();
+	while (strlen(Data[i]) > 0) {
+		get_ip_address(tgt_ip, Data[i]);
+		if (strcmp(tgt_ip, ip) == 0) {
+			int start_min = get_start_time(Data[i]);
+			int end_min = get_end_time(Data[i]);
+			if (now_min >= start_min && now_min <= end_min) return 1;
+		}
+		i++;
+	}
 	return 0;
 }
 
 // デバッグ用
-void debug_print(char data[DATA_LENGTH][STR_LENGTH])
+void debug_print(void)
 {
 	int i = 0;
 	char tmp[STR_LENGTH];
 
-	now_str(tmp);
+	min2str(tmp,now());
 	printf("現在時刻　：%s\n", tmp);
 	printf("/////////////////////////////////\n\n");
-	while (strlen(data[i]) > 0 && i < DATA_LENGTH) {
+	while (strlen(Data[i]) > 0 && i < DAT_LENGTH) {
 		printf("Index:%03d-------------------\n",i);
-		get_ip_address(tmp, data[i]);
+		get_ip_address(tmp, Data[i]);
 		printf("IPアドレス：%s\n", tmp);
-		get_start_time(tmp, data[i]);
+		min2str(tmp, get_start_time(Data[i]));
 		printf("開始時刻　：%s\n", tmp);
-		get_end_time(tmp, data[i]);
+		min2str(tmp, get_end_time(Data[i]));
 		printf("終了時刻　：%s\n", tmp);
 		printf("\n");
 		i++;
 	}
 }
 
-// メインルーチン
-int main(int argc, const char** argv)
+// エントリポイント
+int main(int argc, char** argv)
 {
-	char path[STR_LENGTH];
-	char data[DATA_LENGTH][STR_LENGTH];
 	int ret = 0;
 
-	get_basename(path,argv[0]);
-	strcat_s(path,STR_LENGTH,".ini");
-	load_data(data,path);
-	if (data == NULL) {
-		fprintf(stderr, ERR_STRING, path);
-		ret = 1;
+	if (argc == 2) {
+		char path[STR_LENGTH];
+		get_basename(path, argv[0]);
+		strcat_s(path, STR_LENGTH, ".ini");
+		if (load_data(path)) {
+			fprintf(stderr, ERR_STRING, path);
+		}
+		else {
+			debug_print();
+			ret = chktime(argv[1]);
+		}
 	}
 	else {
-		debug_print(data);
+		fprintf(stderr, USG_STRING);
 	}
 	return ret;
 }
